955ac6fa885db1e9a2ac7d6efe390de0f1b41586
Najeeb Asif <najeeburehman.asif.2019@uni.strath.ac.uk>
2021-01-29 14:55:17 +0000

    Stage 1 possibly done?

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main (void) {
 	// The operation of the shell should be as follows:
@@ -8,7 +11,37 @@ int main (void) {
 	// Save the current path
 	// Load history
 	// Load aliases
+	char input[512];
+	bool exit = false;
+	char* token;
+	int tokenIndex = 0;
+	char* tokenList[10];
+	char delimit[] = "|><&; \t";
 	// Do while shell has not terminated
+	while(!exit){
+		printf("> ");
+		tokenIndex = 0;
+		fgets(input, 512, stdin);
+		// If statement to exit when <ctrl>-D pressed
+		if (feof(stdin) != 0) { 
+			exit = true;
+			printf("\n");
+			break;
+		}
+		token =  strtok(input, delimit);
+		while(token != NULL){
+			printf("%s\n", token);
+			tokenList[tokenIndex++] = token;
+			token = strtok(NULL, delimit);
+		}
+
+		if (strcmp(tokenList[0], "exit") == 0){
+			exit = true;
+		}
+
+		free(token);
+		
+	}
 		// Display prompt
 		// Read and parse user input
 		// While the command is a history invocation or alias then replace it with the
