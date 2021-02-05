3ec0f4f485d84264a1a86a3e68599b3ded494754
Najeeb Asif <najeeburehman.asif.2019@uni.strath.ac.uk>
2021-02-05 18:30:02 +0000

    changed tokenList to tokenArray

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define MAX_INPUT_LENGTH 512
 #define DELIMITERS " \t\n|><&;"
 
-char *tokenList[50];
+char *tokenArray[50];
 
 int getNumberOfTokens(char *input);
 void printTokens(int noOfTokens);
@@ -62,15 +62,15 @@ int main (void) {
 		}
 
 		// If user entered exit with 0 zero arguments then break else error message
-		if((strcmp(tokenList[0], "exit") == 0) && noOfTokens == 1){
+		if((strcmp(tokenArray[0], "exit") == 0) && noOfTokens == 1){
 			exit = true;
 			break;
-		} else if ((strcmp(tokenList[0], "exit") == 0) && noOfTokens > 1){
+		} else if ((strcmp(tokenArray[0], "exit") == 0) && noOfTokens > 1){
 				printf("ERROR: 'exit' does not take any arguments\n");
 				continue;
 		}
 		
-		printTokens(noOfTokens); // Print each token for testing
+		//printTokens(noOfTokens); // Print each token for testing
 
 		// While the command is a history invocation or alias then replace it with the
 		// appropriate command from history or the aliased command respectively
@@ -92,18 +92,18 @@ int getNumberOfTokens(char *input){
 	token = strtok(input, DELIMITERS);
 	int x = 0;
 	while (token != NULL){
-		tokenList[x] = token;
+		tokenArray[x] = token;
 		token = strtok(NULL, DELIMITERS);
 		x++;
 	}
-	tokenList[x] = NULL;
+	tokenArray[x] = NULL;
 	return x;
 }
 
 void printTokens(int noOfTokens){
 	printf("Number of Tokens: %d\n", noOfTokens);
 	for(int i = 0; i < noOfTokens; i++){
-			printf("tokenList[%d]: \"%s\"\n" , i, tokenList[i]);
+			printf("tokenArray[%d]: \"%s\"\n" , i, tokenArray[i]);
 	}
 }
 
@@ -120,8 +120,8 @@ void executeExternalCommand(void){
 		fprintf(stderr, "Fork Failed");
 		return;
 	} else if(pid == 0){
-		execvp(tokenList[0], tokenList);
-		perror(tokenList[0]);
+		execvp(tokenArray[0], tokenArray);
+		perror(tokenArray[0]);
 		exit(EXIT_FAILURE);
 	} else{
 		wait(NULL);
