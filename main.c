65337339bd974052291c057d93408178855d02c4
Najeeb Asif <najeeburehman.asif.2019@uni.strath.ac.uk>
2021-02-06 20:53:37 +0000

    set home dir & restore path

--- main.c
+++ main.c
@@ -6,19 +6,31 @@
 
 #define MAX_INPUT_LENGTH 512
 #define DELIMITERS " \t\n|><&;"
+#define MAX_PATH_LENGTH 512
 
 void tokeinze(char *input, char *tokens[]);
 int countTokens(char *tokens[]);
 void printTokens(int noOfTokens, char *tokens[]);
 void clearInputStream(void);
 void executeExternalCommand(char *tokens[]);
+void getDirectory(char dir[]);
+void restorePath(char *path);
 
 int main (void) {
 	// The operation of the shell should be as follows:
-	
-	// Find the user home directory from the environment
-	// Set current working directory to user home directory
+
 	// Save the current path
+	char *orginalPath = getenv("PATH");
+	printf("Original Path: %s\n", getenv("PATH"));
+	// Find the user home directory from the environment
+	char *home = getenv("HOME");
+	char directory[MAX_PATH_LENGTH];
+	// Set current working directory to user home directory
+	getDirectory(directory);
+	printf("Starting Directory: %s\n", directory);
+	chdir(home);
+	getDirectory(directory);
+	printf("Home Directory Set: %s\n", directory);
 	// Load history
 	// Load aliases
 
@@ -37,6 +49,7 @@ int main (void) {
 		// If statement to exit when <ctrl>-D pressed
 		if (feof(stdin) != 0){
 			printf("\n");
+			restorePath(orginalPath);
 			break;
 		}
 
@@ -58,6 +71,7 @@ int main (void) {
 
 		// If user entered exit with 0 zero arguments then break else error message
 		if((strcmp(tokenArray[0], "exit") == 0) && tokenArray[1] == NULL){
+			restorePath(orginalPath);
 			break;
 		} else if ((strcmp(tokenArray[0], "exit") == 0) && tokenArray[1] != NULL){
 				printf("ERROR: 'exit' does not take any arguments\n");
@@ -69,7 +83,7 @@ int main (void) {
 			continue;
 		}
 		
-		printTokens(noOfTokens, tokenArray); // Print each token for testing
+		//printTokens(noOfTokens, tokenArray); // Print each token for testing
 
 		// While the command is a history invocation or alias then replace it with the
 		// appropriate command from history or the aliased command respectively
@@ -132,4 +146,14 @@ void executeExternalCommand(char *tokens[]){
 	} else{
 		wait(NULL);
 	}
+}
+
+void getDirectory(char dir[]){
+	getcwd(dir, MAX_PATH_LENGTH);//add error
+}
+
+void restorePath(char *path){
+	printf("Path before restore: %s\n", getenv("PATH"));
+	setenv("PATH", path, 1); //add error
+	printf("Path after restore: %s\n", getenv("PATH"));
 }
